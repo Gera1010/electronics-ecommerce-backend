9cf403d634e6e48aa1f97269b63e3501489b08fd
Gera1010 <divyanshgera2017@gmail.com>
2024-11-10 00:39:55 +0530

    updated server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,11 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // You should provide an array of strings for app.keys.
+    keys: env.array('APP_KEYS', [
+      'u7e^BT-VC>c&tAYL`+.5(r',  // Replace with a randomly generated key
+      'vP~M-m$sp;EUdT=(ny9L>]',  // Replace with another random key
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
