490f220a8e6f6e92eedf91f11242159453a72755
Gera1010 <divyanshgera2017@gmail.com>
2024-11-10 00:58:24 +0530

    changer in admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'SXJY6izIFZMQy8J1BpC4Ktn3W0RkWjHggYX8aEowZ6v1Xj3hVXkt9vGBz+36FfB0'), // Replace with your generated ADMIN_JWT_SECRET
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'W89Ytq/XNABgZsaDR9EHyQ=='), // Replace with your generated API_TOKEN_SALT
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'gSkkLdyLNf9L+4ScQtgT2g=='), // Replace with your generated TRANSFER_TOKEN_SALT
     },
   },
   flags: {
