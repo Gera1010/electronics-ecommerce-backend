7f4070dfac6550f27bb3415d30ea055450fe0a37
Gera1010 <divyanshgera2017@gmail.com>
2024-11-10 01:18:02 +0530

    changer in cores backned

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,11 +2,19 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://electronics-ecommerce-frontend-omega.vercel.app'], // Frontend Vercel URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  'strapi::session', // Ensure session configuration is correct in config/server.js
   'strapi::favicon',
   'strapi::public',
 ];
