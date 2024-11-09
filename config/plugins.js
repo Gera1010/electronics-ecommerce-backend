2f17a3a32ddb223a7379db4e81c03d64d1af48c9
Gera1010 <divyanshgera2017@gmail.com>
2024-11-10 00:51:15 +0530

    updated plugin.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+const crypto = require('crypto');
+
+module.exports = () => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: crypto.randomBytes(16).toString('base64'),
+    },
+  },
+});
